aabe626a34305208db5a875cbfaee3e9e97c79a9
Joseph <joseph.larralde@gmail.com>
2018-03-13 06:53:27 +0100

    rendered website

--- src/server/menus.js
+++ src/server/menus.js
@@ -3,13 +3,13 @@ import config from './config/default';
 
 const main = {
   main: {
-    route: `${config.root}`,
+    route: '/',
     text: 'Blog',
     picture: null
   },
   editor: {
-    // route: 'editor',
-    route: path.join(`${config.root}`, 'editor'),
+    route: '/editor',
+    // route: path.join(`${config.root}`, 'editor'),
     text: 'Monitor',
     picture: null
   },
